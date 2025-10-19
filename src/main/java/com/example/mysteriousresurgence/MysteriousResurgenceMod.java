e2f6dbf713efa20e6ef995a33c22a2d50e04bd3f
SudoFish02103343 <yuxin02103343@icloud.com>
2025-10-20 01:54:47 +0800

    Initialize mod and register entities and attributes
    Register items, entities, and their attributes during mod initialization.

--- src/main/java/com/example/mysteriousresurgence/MysteriousResurgenceMod.java
+++ src/main/java/com/example/mysteriousresurgence/MysteriousResurgenceMod.java
@@ -1,6 +1,12 @@
 package com.example.mysteriousresurgence;
 
+import com.example.mysteriousresurgence.entities.*;
+import com.example.mysteriousresurgence.items.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,6 +16,26 @@ public class MysteriousResurgenceMod {
     public static final Logger LOGGER = LogManager.getLogger();
 
     public MysteriousResurgenceMod() {
-        LOGGER.info("神秘复苏模组加载成功！");
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        // 注册物品和实体
+        ModItems.ITEMS.register(modEventBus);
+        ModEntities.ENTITIES.register(modEventBus);
+        
+        // 注册实体属性
+        modEventBus.addListener(this::entityAttributes);
+        
+        MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.info("神秘复苏模组加载成功！现在包含3种鬼魂类型。");
+    }
+    
+    private void entityAttributes(EntityAttributeCreationEvent event) {
+        // 注册所有鬼魂实体的属性
+        event.put(ModEntities.WANDERING_GHOST.get(), 
+            WanderingGhost.createAttributes().build());
+        event.put(ModEntities.KNOCKING_GHOST.get(), 
+            KnockingGhost.createAttributes().build());
+        event.put(ModEntities.GHOST_OFFICER.get(), 
+            GhostOfficer.createAttributes().build());
     }
 }
